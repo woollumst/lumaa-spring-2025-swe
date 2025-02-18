24481c0ad5777f3072aa6faf084dd5e411d948a8
Toby <woollumst@gmail.com>
2025-02-17 22:22:16 -0700

    Update index.js

--- backendTaskManagementApp/src/index.js
+++ backendTaskManagementApp/src/index.js
@@ -3,12 +3,14 @@ import cors from "cors";
 import dotenv from "dotenv";
 import jwt from "jsonwebtoken";
 import client from "./db.js";
+import authRoutes from "./routes/auth.js";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
+app.use("/api/auth", authRoutes); // login, register etc handled im auth.js
 
 app.get('/', async (req, res) => {
     try{
@@ -39,19 +41,6 @@ app.delete('tasks/:id', (req, res) => {
 */
 
 
-// /user            make password hashed string
-// register         use bcrypt for password hashing to store securely
-
-// login            return JWT upon login
-app.post('/api/auth/login', (req, res) => {
-   const { username, password } = req.body;
-   if (username === "admin" && password === "password"){
-      const token = jwt.sign({ username }, process.env.JWT_SECRET, { expiresIn: '1h' });
-      res.json({ token });
-   } else {
-      res.status(401).json({ message: "Invalid credentials" });
-   }
-});
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
