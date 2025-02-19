b24778769ac9a1609dae28060552b959b2383459
Toby <woollumst@gmail.com>
2025-02-18 22:11:22 -0700

    Update taskRepository.js

---
+++ backendTaskManagementApp/src/repository/taskRepository.js
@@ -0,0 +1,22 @@
+import clients from "./db.js";
+
+class taskRepository {
+    constructor(){}
+    getTasks(){
+        
+    }
+    
+    createTask(){
+        
+    }
+    
+    updateTask() {
+        
+    }
+    
+    deleteTask() {
+        
+    }
+}
+
+export default taskRepository;
