e9d85218adeabc27d3c23c63e6800c96340eb9f7
Toby <woollumst@gmail.com>
2025-02-19 18:54:03 -0700

    Update taskRepository.js

--- server/src/repository/taskRepository.js
+++ server/src/repository/taskRepository.js
@@ -3,19 +3,30 @@ import clients from "./db.js";
 class taskRepository {
     constructor(){}
     getTasks(){
-        
+        return client.query(
+            'SELECT * FROM tasks'
+        );
     }
     
-    createTask(){
-        
+    createTask(title, description, isComplete, userId){
+        return client.query( // insert task into database
+            'INSERT INTO tasks (title, description, isComplete, userId) VALUES ($1, $2, $3, $4) RETURNING *',
+            [ title, description, isComplete, userId ]
+        );
     }
     
-    updateTask() {
-        
+    updateTask(id, title, description, isComplete) {
+        return client.query( // update task in database
+            'UPDATE tasks SET title = $1, description = $2, isComplete = $3 WHERE id = $4 RETURNING *',
+            [ title, description, isComplete, id ]
+        );
     }
     
-    deleteTask() {
-        
+    deleteTask(id) {
+        client.query(
+            'DELETE FROM tasks WHERE id = $1',
+            [ id ]
+        );
     }
 }
 
