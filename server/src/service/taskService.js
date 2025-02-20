f11fdf63d156ed39d31a67b905b2fc818439dc00
Toby <woollumst@gmail.com>
2025-02-19 19:00:51 -0700

    Update taskService.js

--- server/src/service/taskService.js
+++ server/src/service/taskService.js
@@ -3,19 +3,21 @@ import taskRepository from "../repository/taskRepository.js";
 class taskService {
     constructor(){}
     getTasks() {
-        
+        return taskRepository.getTasks();
     }
     
-    createTask () {
-        
+    createTask (task) {
+        const {title, description, isComplete, userId} = task;
+        return await taskRepository.createTask(title, description, isComplete, userId);
     }
     
-    updateTask () {
-        
+    updateTask (task) {
+        const {id, title, description, isComplete, userId } = task;
+        return await taskRepository.updateTask(id, title, description, isComplete);
     }
     
-    deleteTask () {
-        
+    deleteTask (id) {
+        taskRepository.deleteTask(id);
     }
 }
 
