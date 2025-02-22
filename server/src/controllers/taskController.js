371364f9debf59d3044e685c7ce9e43c7759ae49
Toby <woollumst@gmail.com>
2025-02-22 08:39:50 -0700

    Update taskController.js

--- server/src/controllers/taskController.js
+++ server/src/controllers/taskController.js
@@ -5,55 +5,63 @@ import authenticate from "../middleware/authMiddleware.js";
 
 const taskRoutes = express.Router();
 
-// /tasks           verify JWT upon running protected tasks
+// /tasks endpoints
+// todo: setup result object for task infrastructure so it can return success property?
 
-//get task list
+// Get Task List
 taskRoutes.get('/', authenticate, async (req, res) => {
     try{
         const result = await taskService.getTasks();
-        res.json(result); // Fix?
+        if(!result.success){
+            res.status(404).json({ error: 'Failed to fetch task list' });
+        }
+        res.status(200).json({ result }); // Fix?
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: 'Failed to get tasks' });
+        res.status(500).json({ success: false, message: 'Error getting task list', error: 'Failed to get tasks' });
     }
 });
 
-//create a new task
+// Create a New Task
 taskRoutes.post('', authenticate, (req, res) => {
     const task = req.body;
     try{
         const result = taskService.createTask(task);
-        res.status(201).json(result);
+        if(!result.success){
+            res.status(404).json({ error : 'Failed to create task' });
+        }
+        res.status(201).json({ result });
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error: 'Failed to create task' });
+        res.status(500).json({ succes: false, message: 'Error creating task', error: 'Failed to create task' });
     } 
 });
 
-//update a task
+// Update a Task by ID
 taskRoutes.put('/:id', authenticate, (req, res) => { 
     const { id } = req.params;
     const task = req.body;
     try{
-        const updatedTask = taskService.updateTask(id, task);
-        if(!updatedTask){
+        const result = taskService.updateTask(id, task);
+        if(!result.success){
             res.status(404).json({ error : 'Task not found' });
         }
-        res.json(updatedTask);
+        res.status(202).json({ result });
     } catch (error) {
         console.error(error);
         res.status(500).json({ error : 'Failed to update task' });
     }
 });
 
+// Delete Task by ID
 taskRoutes.delete('/:id', authenticate, (req, res) => { 
     const { id } = req.params;
     try{
-        const deletedTask = taskService.deleteTask(id);
-        if(!deletedTask){
+        const result = taskService.deleteTask(id);
+        if(!result.success){
             res.status(404).json({ error : 'Task not found' });
         }
-        res.json(deletedTask);
+        res.status(202).json({ result });
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Failed to delete task' });
