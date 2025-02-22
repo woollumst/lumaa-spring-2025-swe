f15cf54b5920d65da4e163f9178189eec2d8cb6b
Toby <woollumst@gmail.com>
2025-02-21 20:27:00 -0700

    Update authController.js

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -8,26 +8,24 @@ authRoutes.post("/register", async (req, res) => {
     
     console.log('attempting to register...');
 
-    try{
+    try{ // result = { success, message, user, token }
         const result = authService.authRegister(username, password); // call service layer to handle registration
-        console.log('User registered successfully');
-        console.log(result.json);
-        res.status(201).json({ user: result.rows[0] }); // registration successful
+        res.status(201).json({ result }); // registration successful
     } catch (error) {
         console.error(error);
-        res.status(500).json({ message: 'Error registering user', error: error.message });
+        res.status(500).json({ success: false, message: 'Error registering user', error: error.message });
     }
 });
 
 authRoutes.post("/login", async (req, res) => {
     const { username, password } = req.body; //accept user input for username/password
 
-    try{
-        const token = authService.handleLogin(username, password); // handle login, generate token upon success
-        res.json({ token }); // complete login, send token
+    try{ // result = { success, message, user, token }
+        const resilt = authService.handleLogin(username, password); // handle login, generate token upon success
+        res.json({ result }); // complete login, send token
     } catch(error) {
         console.error(error);
-        res.status(500).json({ message: 'Error logging in', error: error.message });
+        res.status(500).json({ success: false, message: 'Error logging in', error: error.message });
     }
 });
 
