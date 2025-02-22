3bdcd7eedb0b414a8066bcb6e5ac924dfa286ca3
Toby <woollumst@gmail.com>
2025-02-21 20:12:46 -0700

    Update authRepository.js

--- server/src/repository/authRepository.js
+++ server/src/repository/authRepository.js
@@ -2,13 +2,30 @@ import client from "./db.js";
 
 export const authRepository = {
     register (username, hashedPassword) {
-        return client.query( // register user to user database
-            'INSERT INTO userdb (username, password) VALUES ($1, $2) RETURNING *',
-            [ username, hashedPassword ]
-        );
+        try{
+            const result = client.query( // register user to user database
+                'INSERT INTO userdb (username, password) VALUES ($1, $2) RETURNING *',
+                [ username, hashedPassword ]
+            );
+            return result.rows[0];
+        } catch (error) {
+            console.error('Database error');
+            throw new Error('Database query failed');
+        }
     },
 
     async getByUsername (username) {  // pull user from db to handle login
-        return await client.query('SELECT * FROM userdb WHERE username=$1', [username]);
+        try{
+            const result = await client.query(
+                'SELECT username, password 
+                FROM userdb 
+                WHERE username=$1', 
+                [username]
+            );
+            return result.rows[0];
+        } catch (error) {
+            console.error('Database error');
+            throw new Error('Database query failed');
+        }
     }
 };
