5b31a0fe7efb95b598b3cd60c7df776cab72c095
Toby <woollumst@gmail.com>
2025-02-23 12:18:11 -0700

    Update authRepository.js

--- server/src/repository/authRepository.js
+++ server/src/repository/authRepository.js
@@ -4,7 +4,7 @@ export const authRepository = {
     register (username, hashedPassword) {
         try{
             const result = client.query( // register user to user database
-                'INSERT INTO userdb (username, password) VALUES ($1, $2) RETURNING *',
+                'INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *',
                 [ username, hashedPassword ]
             );
             return result.rows[0];
@@ -17,7 +17,7 @@ export const authRepository = {
     async getByUsername (username) {  // pull user from db to handle login
         try{
             const result = await client.query(
-                'SELECT username, password FROM userdb WHERE username=$1', 
+                'SELECT username, password FROM users WHERE username=$1', 
                 [username]
             );
             return result.rows[0];
